eb365d056349a6c78e1b71d7d7b6525539bef7f4
adir-avraham <adir.avraham@gmail.com>
2019-09-04 11:28:43 +0300

    add-get-api2-flip

--- index.js
+++ index.js
@@ -18,13 +18,15 @@ function getCurrenciesNameSymble(currencies) {
 function draw(currenciesNameSymbol) {
     currenciesNameSymbol.forEach((currency, index, currenciesNameSymbol) => {
         const clonedCard = $("#coinCard").clone();
+        clonedCard.css({ display: "inline-block" });
         clonedCard.find("h5").html(currenciesNameSymbol[index].name);
         clonedCard.find("p").html(currenciesNameSymbol[index].symbol);
         clonedCard.find("input").attr("id", currenciesNameSymbol[index].name);
-        clonedCard.find(".infoBtn").attr("alt", currenciesNameSymbol[index].name);   
-        clonedCard.find(".infoBtn").on("click", function(event) {
-            searchinfo(event.target.alt.toLowerCase())
-        })   
+        clonedCard.find(".infoBtn").attr("alt", currenciesNameSymbol[index].name);
+        clonedCard.find(".infoBtn").on("click", function (event) {
+            searchinfo(event.target.alt.toLowerCase(), event)
+            //console.log(event.toElement.parentElement.parentElement)
+        })
         clonedCard.find("label").attr("for", currenciesNameSymbol[index].name);
         $("#divCoins").append(clonedCard);
 
@@ -35,30 +37,47 @@ function draw(currenciesNameSymbol) {
 
 
 
- 
-    function searchinfo (currency) {
 
-        api.getCurrencyInfoById(currency).then((result) => {
-        
-            const image = result.image.small;
-            const current_price_usd = result.market_data.current_price.usd;
-            const current_price_eur = result.market_data.current_price.eur;
-            const current_price_ils = result.market_data.current_price.ils;
+function searchinfo(currency, event) {
+    const coinCard = event.toElement.parentElement.parentElement;
+
+    api.getCurrencyInfoById(currency).then((result) => {
+
+        const image = result.image.small;
+        const current_price_usd = result.market_data.current_price.usd;
+        const current_price_eur = result.market_data.current_price.eur;
+        const current_price_ils = result.market_data.current_price.ils;
+
+
+
+
+
+        console.log(result)
+        drawInfo(image)
+        //console.log(image)
+        console.log(current_price_usd + "$")
+        console.log(current_price_eur + "EUR")
+        console.log(current_price_ils + "ILS")
+
+    })
+
+    //$(this).toggleClass("bg-silver-coin");
+    function drawInfo(image) {
+        $(coinCard).toggleClass("bg-silver-coin");
+        console.log(image);
+        setTimeout(function(){ 
+            $(coinCard).find("h5").html(`<img src=${image}>`);          
+             }, 1600);
+  
             
-
-            console.log(result)
-
-            console.log(image)           
-            console.log(current_price_usd + "$")
-            console.log(current_price_eur + "EUR")
-            console.log(current_price_ils + "ILS" )
-            
-        })
-        
-
-
+        // const coinDiv = event.target.parentElement.parentElement;
+        // $(coinDiv).toggleClass("bg-silver-coin");
 
     }
+
+}
+
+
 
 
 
@@ -85,22 +104,22 @@ function searchinfo (e) {
     console.log(this)
     const currency = "bitcoin"
     api.getCurrencyInfoById(currency).then((result) => {
-    
+
         const image = result.image.small;
         const current_price_usd = result.market_data.current_price.usd;
         const current_price_eur = result.market_data.current_price.eur;
         const current_price_ils = result.market_data.current_price.ils;
-        
+
 
         console.log(result)
 
-        console.log(image)           
+        console.log(image)
         console.log(current_price_usd + "$")
         console.log(current_price_eur + "EUR")
         console.log(current_price_ils + "ILS" )
-        
+
     })
-    
+
 
 
 
