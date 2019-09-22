cac1b8744ac07e945bf7c3f92644e9bd621a72e7
adir-avraham <adir.avraham@gmail.com>
2019-09-23 00:28:51 +0300

    spinner+improvements

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function init() {
     emptyDivCoins()
     spinner("divCoins")
     api.getCurrencies().then((result) => {
-        currencies = result.slice(0, 100)
+        currencies = result.slice(0, 15)
         state = [ ...currencies ];
         draw(state);
         clearArray();
@@ -25,7 +25,11 @@ $("#divCoins").empty();
 function spinner(idElement) {
     const spinner = $("#spinner").clone();
     spinner.css({ display: "inline-block" });
+    if ($(`#${idElement}`).find("#spinner").length > 0 ) {
+        $(`#${idElement}`).find("#spinner").show()
+    } else {
     $(`#${idElement}`).append(spinner);
+    }
 }
 
 
@@ -70,28 +74,28 @@ function  clearArray() {
 function draw(currenciesArray) {
     emptyDivCoins()
 
-    currenciesArray.forEach((currency, index) => {
+    currenciesArray.forEach((currency) => {
         const clonedCard = $("#coinCard").clone();
         clonedCard.css({ display: "inline-block" });
-        clonedCard.find("#name").html(currenciesArray[index].name);
-        clonedCard.find("h5").html(currenciesArray[index].symbol.toUpperCase());
+        clonedCard.find("#name").html(currency.name);
+        clonedCard.find("h5").html(currency.symbol.toUpperCase());
 
-        clonedCard.find("input").attr("id", currenciesArray[index].symbol);
-        clonedCard.find("input").attr("value", currenciesArray[index].symbol);
+        clonedCard.find("input").attr("id", currency.symbol);
+        clonedCard.find("input").attr("value", currency.symbol);
         clonedCard.find("input").on("change", checkedCurrency)
         clonedCard.find("input").on("click", checkMaxCurrency)
 
-        clonedCard.find(".infoBtn").attr("id", currenciesArray[index].id);
-        clonedCard.find(".infoBtn").attr("data-target", `#a${currenciesArray[index].id}`);
-        clonedCard.find(".infoBtn").attr("aria-controls", `a${currenciesArray[index].id}`);
+        clonedCard.find(".infoBtn").attr("id", currency.id);
+        clonedCard.find(".infoBtn").attr("data-target", `#a${currency.id}`);
+        clonedCard.find(".infoBtn").attr("aria-controls", `a${currency.id}`);
         clonedCard.find(".infoBtn").on("click", function (event) {
             searchinfo(event.target.id.toLowerCase(), event)
             //optional =>>>>>   $(this).parents(".bg-coin").toggleClass("bg-silver-coin");   
         })
         
-        clonedCard.find(".collapse").attr("id", `a${currenciesArray[index].id}`);
+        clonedCard.find(".collapse").attr("id", `a${currency.id}`);
         
-        clonedCard.find("label").attr("for", currenciesArray[index].symbol);
+        clonedCard.find("label").attr("for", currency.symbol);
         
 
         $("#divCoins").append(clonedCard);
@@ -102,18 +106,22 @@ function draw(currenciesArray) {
 
 
 function searchinfo(currency, event) {
-    //spinner(`a${currency}`);
-    //$(`#a${currency}`).empty()    
+    
+    if (!$(`#a${currency}`).hasClass("show")) {
+        spinner(`a${currency}`); 
+    }
+    
     const coinCard = event.toElement.parentElement.parentElement;
     
     api.getCurrencyInfoById(currency).then((result) => {
-
+        
         const image = result.image.small;
         const current_price_usd = result.market_data.current_price.usd;
         const current_price_eur = result.market_data.current_price.eur;
         const current_price_ils = result.market_data.current_price.ils;
-
+        
         drawInfo(image, current_price_usd, current_price_eur, current_price_ils, coinCard )
+        $("#spinner").remove()    
   
     }).catch(err => noDadaMessage())   
 }
@@ -133,7 +141,8 @@ function drawInfo(image, current_price_usd, current_price_eur, current_price_ils
 
 
 function noDadaMessage() {
-    alert("No data")
+    alert("No data");
+    $("#spinner").remove();
 }
 
 
@@ -172,9 +181,9 @@ function openModal() {
     selectedCurrency.forEach((currency, index) => {
         const listItem = $("#listItem").clone()
         listItem.css({ visibility: "visible" });
-        listItem.find("span").text(selectedCurrency[index]);
+        listItem.find("span").text(currency);
         listItem.find("input").attr("id", `item-${index}`); 
-        listItem.find("input").attr("value", selectedCurrency[index]);
+        listItem.find("input").attr("value", currency);
         listItem.find("label").attr("for", `item-${index}`);
 ///change here as a test  "checkedCurrency" insted of "checkedCurrencyInModal"
         listItem.find("input").on("change", checkedCurrency)
@@ -220,31 +229,28 @@ function saveChangesModal() {
 
 
 function liveReportsPage() {
-        emptyDivCoins()
-        //spinner("divCoins")
+
+        emptyDivCoins();
+        spinner("divCoins");
+    
         api.getCurrenciesPrice(selectedCurrency[0], selectedCurrency[1], selectedCurrency[2], selectedCurrency[3], selectedCurrency[4]).then((result) => {
-            
-        //console.log("current =", current_usd_rate_0)
-        console.log("result =>",  result)
-            
-        // const current_usd_rate_0 = result[selectedCurrency[0]].USD;
-        // console.log("current =", current_usd_rate_0)
-        // const current_usd_rate_1 = result[selectedCurrency[1]].USD;
-        // const current_usd_rate_2 = result[selectedCurrency[2]].USD;
-        // const current_usd_rate_3 = result[selectedCurrency[3]].USD;
-        // const current_usd_rate_4 = result[selectedCurrency[4]].USD;
         
-        
-        
+        const current_usd_rate_0 = result[selectedCurrency[0]] ? result[selectedCurrency[0]].USD : 0;
+        const current_usd_rate_1 = result[selectedCurrency[1]] ? result[selectedCurrency[1]].USD : 0;
+        const current_usd_rate_2 = result[selectedCurrency[2]] ? result[selectedCurrency[2]].USD : 0;
+        const current_usd_rate_3 = result[selectedCurrency[3]] ? result[selectedCurrency[3]].USD : 0;
+        const current_usd_rate_4 = result[selectedCurrency[4]] ? result[selectedCurrency[4]].USD : 0;
         
         const cloneCard = $("#chartContainer").clone();
         cloneCard.css({display: "inline-block"})
         $("#divCoins").append(cloneCard)
-        //renderChart(current_usd_rate_0, current_usd_rate_1, current_usd_rate_2, current_usd_rate_3, current_usd_rate_4)
-        renderChart()
         
-    }).catch(err => noDadaMessage())
-      
+        renderChart(current_usd_rate_0, current_usd_rate_1, current_usd_rate_2, current_usd_rate_3, current_usd_rate_4)
+        
+        $("#spinner").remove() 
+        
+    }).catch(err => noDadaMessage()) 
+
 }
 
 
@@ -259,16 +265,16 @@ function renderChart(current_usd_rate_0, current_usd_rate_1, current_usd_rate_2,
 			// Change type to "doughnut", "line", "splineArea", etc.
 			type: "column",
 			dataPoints: [
-				{ label: selectedCurrency[0].toLowerCase(),  y: 5 },
-				{ label: selectedCurrency[1], y: 3 },
-				{ label: selectedCurrency[2], y: 10 },
-				{ label: selectedCurrency[3],  y: 1 },
-				{ label: selectedCurrency[4],  y: 20 }
+				{ label: selectedCurrency[0], y: current_usd_rate_0 },
+				{ label: selectedCurrency[1], y: current_usd_rate_1 },
+				{ label: selectedCurrency[2], y: current_usd_rate_2 },
+				{ label: selectedCurrency[3], y: current_usd_rate_3 },
+				{ label: selectedCurrency[4], y: current_usd_rate_4 }
 			]
 		}
 		]
 	});
-	chart.render();
+    chart.render();
 }
 
 
@@ -279,7 +285,6 @@ function aboutPage() {
     const cloneCard = $("#aboutContent").clone();
     cloneCard.css({display: "inline-block"})
     $("#divCoins").append(cloneCard)
-
 }
 
 
